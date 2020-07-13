800db3769e34600b0023465c767f54fd57e6a0c0
Itumeleng091slk <magdalene.selokela@umuzi.org>
2020-07-13 14:34:00 +0200

    re-wrote instructions

--- Database_sql.sql
+++ Database_sql.sql
@@ -1,399 +1,251 @@
---
--- PostgreSQL database dump
---
+-- PART 1
+-- Creating umuzi database
 
--- Dumped from database version 9.6.17
--- Dumped by pg_dump version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: umuzischema; Type: SCHEMA; Schema: -; Owner: user
---
+CREATE DATABASE umuzi;
 
 CREATE SCHEMA umuzischema;
 
+CREATE TABLE customers (
+    customer_id INTEGER NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(10),
+    address VARCHAR(200),
+    phone VARCHAR(15),
+    email VARCHAR(100),
+    city VARCHAR(20),
+    country VARCHAR(50)
+);
 
-ALTER SCHEMA umuzischema OWNER TO "user";
+CREATE TABLE employees (
+    employee_id INTEGER NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    job_title VARCHAR(20)
+);
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: customers; Type: TABLE; Schema: public; Owner: user
---
-
-CREATE TABLE public.customers (
-    customer_id integer NOT NULL,
-    first_name character varying(50),
-    last_name character varying(50),
-    gender character varying(10),
-    address character varying(200),
-    phone character varying(15),
-    email character varying(100),
-    city character varying(20),
-    country character varying(50)
+CREATE TABLE orders (
+    order_id INTEGER NOT NULL,
+    date_required DATE,
+    date_shipped DATE,
+    status VARCHAR(20),
+    productid INTEGER,
+    paymentid INTEGER,
+    fullfilled_by_employeeid INTEGER
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (payment_id) REFERENCES payments(payment_id),
+    FOREIGN KEY (fullfilled_by_employee_id) REFERENCES employees(employee_id)
 );
 
 
-ALTER TABLE public.customers OWNER TO "user";
-
---
--- Name: customers_customer_id_seq; Type: SEQUENCE; Schema: public; Owner: user
---
-
-CREATE SEQUENCE public.customers_customer_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.customers_customer_id_seq OWNER TO "user";
-
---
--- Name: customers_customer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
---
-
-ALTER SEQUENCE public.customers_customer_id_seq OWNED BY public.customers.customer_id;
-
-
---
--- Name: employees; Type: TABLE; Schema: public; Owner: user
---
-
-CREATE TABLE public.employees (
-    employee_id integer NOT NULL,
-    first_name character varying(50),
-    last_name character varying(50),
-    email character varying(100),
-    job_title character varying(20)
+CREATE TABLE payments (
+    payment_id INTEGER NOT NULL,
+    payment_date DATE,
+    amount NUMERIC,
+    customerid INTEGER
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
 
 
-ALTER TABLE public.employees OWNER TO "user";
-
---
--- Name: employees_employee_id_seq; Type: SEQUENCE; Schema: public; Owner: user
---
-
-CREATE SEQUENCE public.employees_employee_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.employees_employee_id_seq OWNER TO "user";
-
---
--- Name: employees_employee_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
---
-
-ALTER SEQUENCE public.employees_employee_id_seq OWNED BY public.employees.employee_id;
-
-
---
--- Name: orders; Type: TABLE; Schema: public; Owner: user
---
-
-CREATE TABLE public.orders (
-    order_id integer NOT NULL,
-    date_required date,
-    date_shipped date,
-    status character varying(20),
-    productid integer,
-    paymentid integer,
-    fullfilled_by_employeeid integer
+CREATE TABLE products (
+    product_id INTEGER NOT NULL,
+    product_name VARCHAR(100),
+    description VARCHAR(300),
+    buying_price NUMERIC
 );
 
-
-ALTER TABLE public.orders OWNER TO "user";
-
---
--- Name: orders_order_id_seq; Type: SEQUENCE; Schema: public; Owner: user
---
-
-CREATE SEQUENCE public.orders_order_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.orders_order_id_seq OWNER TO "user";
-
---
--- Name: orders_order_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
---
-
-ALTER SEQUENCE public.orders_order_id_seq OWNED BY public.orders.order_id;
-
-
---
--- Name: payments; Type: TABLE; Schema: public; Owner: user
---
-
-CREATE TABLE public.payments (
-    payment_id integer NOT NULL,
-    payment_date date DEFAULT ('now'::text)::date NOT NULL,
-    amount numeric,
-    customerid integer
-);
-
-
-ALTER TABLE public.payments OWNER TO "user";
-
---
--- Name: payments_payment_id_seq; Type: SEQUENCE; Schema: public; Owner: user
---
-
-CREATE SEQUENCE public.payments_payment_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.payments_payment_id_seq OWNER TO "user";
-
---
--- Name: payments_payment_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
---
-
-ALTER SEQUENCE public.payments_payment_id_seq OWNED BY public.payments.payment_id;
-
-
---
--- Name: products; Type: TABLE; Schema: public; Owner: user
---
-
-CREATE TABLE public.products (
-    product_id integer NOT NULL,
-    product_name character varying(100),
-    description character varying(300),
-    buying_price numeric
-);
-
-
-ALTER TABLE public.products OWNER TO "user";
-
---
--- Name: products_product_id_seq; Type: SEQUENCE; Schema: public; Owner: user
---
-
-CREATE SEQUENCE public.products_product_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.products_product_id_seq OWNER TO "user";
-
---
--- Name: products_product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
---
-
-ALTER SEQUENCE public.products_product_id_seq OWNED BY public.products.product_id;
-
-
---
--- Name: customers customer_id; Type: DEFAULT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.customers ALTER COLUMN customer_id SET DEFAULT nextval('public.customers_customer_id_seq'::regclass);
-
-
---
--- Name: employees employee_id; Type: DEFAULT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.employees ALTER COLUMN employee_id SET DEFAULT nextval('public.employees_employee_id_seq'::regclass);
-
-
---
--- Name: orders order_id; Type: DEFAULT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.orders ALTER COLUMN order_id SET DEFAULT nextval('public.orders_order_id_seq'::regclass);
-
-
---
--- Name: payments payment_id; Type: DEFAULT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.payments ALTER COLUMN payment_id SET DEFAULT nextval('public.payments_payment_id_seq'::regclass);
-
-
---
--- Name: products product_id; Type: DEFAULT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.products ALTER COLUMN product_id SET DEFAULT nextval('public.products_product_id_seq'::regclass);
-
-
---
--- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: user
---
-
-COPY public.customers (customer_id, first_name, last_name, gender, address, phone, email, city, country) FROM stdin;
-4	Leon	Glen	Male	81 Everton Rd,Gillits	0820832830	Leon@gmail.com	Durban	South Africa
-6	Julia	Stein	Female	22 Wernerring	+448672445058	Js234@yahoo.com	Frankfurt	Germany
-2	Lerato	Mabitso	Male	284 chaucer st	084789657	john@gmail.com	Johannesburg	South Africa
-5	Charl	Muller	M￼ale	290A Dorset Ecke	+44856872553	Charl.muller@yahoo.com	Berlin	Germany
-\.
-
-
---
--- Data for Name: employees; Type: TABLE DATA; Schema: public; Owner: user
---
-
-COPY public.employees (employee_id, first_name, last_name, email, job_title) FROM stdin;
-1	Kani	Matthew	mat@gmail.com	Manager
-2	Lesly	Cronje	LesC@gmail.com	Clerk
-3	Gideon	Maduku	m@gmail.com	Accountant
-\.
-
-
---
--- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: user
---
-
-COPY public.orders (order_id, date_required, date_shipped, status, productid, paymentid, fullfilled_by_employeeid) FROM stdin;
-2	2018-04-09	2018-03-09	Shipped	1	2	2
-1	2018-05-09	\N	not shipped	1	1	2
-3	2018-06-09	\N	not shipped	3	3	3
-\.
-
-
---
--- Data for Name: payments; Type: TABLE DATA; Schema: public; Owner: user
---
-
-COPY public.payments (payment_id, payment_date, amount, customerid) FROM stdin;
-1	2018-01-09	150.75	1
-2	2018-03-09	150.75	5
-3	2018-03-09	700.60	4
-\.
-
-
---
--- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: user
---
-
-COPY public.products (product_id, product_name, description, buying_price) FROM stdin;
-1	Harley Davidson Chopper	This replica features working kickstand, front suspension, gear-shift lever	150.75
-2	Classic Car	Turnable front wheels, steering function	550.75
-3	Sports car	Turnable front wheels, steering function	700.60
-\.
-
-
---
--- Name: customers_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
---
-
-SELECT pg_catalog.setval('public.customers_customer_id_seq', 6, true);
-
-
---
--- Name: employees_employee_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
---
-
-SELECT pg_catalog.setval('public.employees_employee_id_seq', 3, true);
-
-
---
--- Name: orders_order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
---
-
-SELECT pg_catalog.setval('public.orders_order_id_seq', 3, true);
-
-
---
--- Name: payments_payment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
---
-
-SELECT pg_catalog.setval('public.payments_payment_id_seq', 3, true);
-
-
---
--- Name: products_product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
---
-
-SELECT pg_catalog.setval('public.products_product_id_seq', 3, true);
-
-
---
--- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.customers
-    ADD CONSTRAINT customers_pkey PRIMARY KEY (customer_id);
-
-
---
--- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.employees
-    ADD CONSTRAINT employees_pkey PRIMARY KEY (employee_id);
-
-
---
--- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.orders
-    ADD CONSTRAINT orders_pkey PRIMARY KEY (order_id);
-
-
---
--- Name: payments payments_pkey; Type: CONSTRAINT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.payments
-    ADD CONSTRAINT payments_pkey PRIMARY KEY (payment_id);
-
-
---
--- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.products
-    ADD CONSTRAINT products_pkey PRIMARY KEY (product_id);
-
-
---
--- PostgreSQL database dump complete
---
+INSERT INTO customers(first_name, last_name, gender, address, phone, email, city, country)
+VALUES(
+        'Leon', 
+        'Glen', 
+        'Male', 
+        '81 Everton Rd,Gillits',
+        '0820832830',
+        'Leon@gmail.com',
+        'Durban',
+        'South Africa'
+     ),
+    (
+        'Julia', 
+        'Stein', 
+        'Female', 
+        '22 Wernerring',
+        '+448672445058',
+        'Js234@yahoo.com',
+        'Frankfurt',
+        'Germany'
+    ),
+    (
+        'Lerato', 
+        'Mabitso', 
+        'Male', 
+        '284 chaucer st',
+        '084789657',
+        'john@gmail.com',
+        'Johannesburg',
+        'South Africa'
+    ),
+    (
+        'Charl', 
+        'Muller', 
+        'Male', 
+        '90A Dorset Ecke',
+        '+44856872553',
+        'Charl.muller@yahoo.com',
+        'Berlin',
+        'Germany'
+    );
+INSERT INTO employees(first_name,last_name,email,job_title) 
+VALUES (
+        'Kani',
+        'Matthew',
+        'mat@gmail.com',
+        'Manager'
+    ),
+    (
+        'Lesly',
+        'Cronje',
+        'LesC@gmail.com',
+        'Clerk'
+    ),
+    (
+        'Gideon',
+        'Maduku',
+        'm@gmail.com',
+        'Accountant'
+    );
+INSERT INTO orders(product_id,payment_id,fullfilled_by_employee_id,date_required,date_shipped,status) 
+VALUES (
+        1,
+        1,
+        2,
+        '05-09-2018',
+        DEFAULT,
+        'Not shipped'
+    ),
+    (
+        1,
+        2,
+        2,
+        '04-09-2018',
+        '03-09-2018',
+        'Shipped'
+    ),
+    (
+        3,
+        3,
+        3,
+        '06-09-2018',
+        DEFAULT,
+        'Not shipped'
+    );
+INSERT INTO payments (customer_id,payment_date,amount,
+        )
+VALUES (
+        1,
+        '01-09-2018',
+        150.75
+    ),
+    (
+        5,
+        '03-09-2018',
+        150.75
+    ),
+    (
+        4,
+        '03-09-2018',
+        700.60
+    );
+INSERT INTO products((product_id,product_name,description,buy_price)
+VALUES (
+        'Harley Davidson Chopper',
+        'This replica features working kickstand, 
+        front suspension, gear-shift lever',
+        150.75
+    ),
+    (
+        'Classic Car',
+        'Turnable front wheels, steering function',
+        550.75
+    ),
+    (
+        'Sports Car',
+        'Turnable front wheels, steering function',
+        700.60
+    );
+
+-- PART 2
+
+-- 1. 
+SELECT * FROM customers;
+
+-- 2.
+SELECT first_name FROM customers;
+
+-- 3.
+SELECT first_name FROM customers
+WHERE customer_id = 1;
+
+-- 4.
+UPDATE customers
+SET first_name = 'Lerato',
+last_name = 'Mabitso'
+WHERE customer_id = 1;
+
+-- 5.
+DELETE FROM customers
+WHERE customer_id = 2
+
+-- 6.
+SELECT COUNT(DISTINCT status)
+FROM orders;
+
+-- 7.
+SELECT MAX(amount)
+FROM payments;
+
+-- 8.
+SELECT * FROM customers
+ORDER BY country;
+
+-- 9.
+SELECT * FROM products
+WHERE buy_price BETWEEN 100 AND 600;
+
+-- 10.
+SELECT * FROM customers
+WHERE country = 'Germany' AND city = 'Berlin';
+
+-- 11.
+SELECT * FROM customers 
+WHERE city = 'Cape Town' OR city = 'Durban';
+
+-- 12.
+SELECT * FROM products
+WHERE buy_price > 500;
+
+-- 13.
+SELECT SUM(amount)
+FROM payments;
+
+-- 14
+SELECT COUNT(status)
+FROM orders
+WHERE status = 'Shipped';
+
+-- 15.
+SELECT AVG(buy_price) AS average_price_rands, 
+AVG(buy_price / 12) AS average_price_dollars
+FROM products;
+
+-- 16.
+SELECT * FROM payments
+INNER JOIN customers
+ON payments.payment_id = customers.customer_id;
+
+-- 17.
+SELECT * FROM products
+WHERE description
+LIKE '%Turnable front wheels%';
 
---
+++ README.md
@@ -0,0 +1,13 @@
+SQL
+
+Step 1 : Create your virtual environment
+-- mkvirtualenv -p $(which python3.7) name of your environment
+
+
+Step 2 : 
+Install Flask
+-- pip install Flask
+
+Step 3 :
+Docker compose
+-- docker compose up
